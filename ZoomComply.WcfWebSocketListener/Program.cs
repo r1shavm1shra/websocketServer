20cffcadd576d8906f17a91984248ad0730826f7
Rishav Mishra <rishav_1995@yahoo.co.in>
2022-11-28 22:49:37 -0500

    Websocket server

---
+++ ZoomComply.WcfWebSocketListener/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.ServiceModel.WebSockets;
+
+namespace OsuExam.WcfWebSocketListener
+{
+    class Program
+    {
+   
+        static void Main(string[] args)
+        {
+            var host = new WebSocketHost<MyWebSocketService>(new Uri("http://localhost:80/WebSocket"));
+            //var bindingSsl = WebSocketHost.CreateWebSocketBinding(true);
+            var endpoint = host.AddWebSocketEndpoint(Int32.MaxValue, Int32.MaxValue, null);
+            
+            host.Open();
+
+            Console.ReadLine();
+
+            host.Close();
+        }
+    }
+}
+
---
+++ ZoomComply.WcfWebSocketListener/WebSocketService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ServiceModel.WebSockets;
+using Microsoft.Web.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net.WebSockets;
+using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OsuExam.WcfWebSocketListener
+{
+    
+    public class MyWebSocketService : WebSocketService
+    {
+        WebSocketCollection _webSocketCollection = new WebSocketCollection();
+        static WebSocketCollection<MyWebSocketService> _sessions = new WebSocketCollection<MyWebSocketService>();
+        
+        
+        public override void OnOpen()
+        {
+            _sessions.Add(this);
+            base.OnOpen();
+        }
+        public override void OnMessage(string message)
+        {
+            if(_sessions.Remove(this))
+            {
+                _sessions.Add(this);
+                _sessions.Broadcast(String.Format("{0}", message));
+            }
+        }
+        protected override void OnClose()
+        {
+            _sessions.Remove(this);
+            this.Dispose(true);
+        }
+        
+    }
+}
